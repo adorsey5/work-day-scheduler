fb53b8fe7bb88794f249f307b5afa2728ce615b2
A. Dorsey <ashadorsey@gmail.com>
2022-01-30 16:09:16 -0500

    added js, jQueries, DOM, and local storage to the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,64 @@
  $("#currentDay").text(currentDay);
 
  $(document).ready(function () {   
+    
+    //displays current date
+    // $("#currentDay").text(moment().format("dddd, MMMM Do"));
    
     //defined the function
     function timeTracer(){
 
         //Generates the current time (from moment library)
         var currentTime = moment().hour();
+        
+        // the time block changes color based on the current hour
+        $(".time-block").each(function() {
+            // colors every time block 
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+            //console.log( timeBlock, currentTime)
+            
+            //colors the time blocks as  present, past, & future.
+            if (timeBlock === currentTime){
+                $(this).addClass("present");
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+            }
+            else if (timeBlock < currentTime) {
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).addClass("future");
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+            }
+        })
+    }
+            //Retrieves the saved task from local Storage
+            $("#hour7 .task-item").val(localStorage.getItem("hour7"));
+            $("#hour8 .task-item").val(localStorage.getItem("hour8"));
+            $("#hour9 .task-item").val(localStorage.getItem("hour9"));
+            $("#hour10 .task-item").val(localStorage.getItem("hour10"));
+            $("#hour11 .task-item").val(localStorage.getItem("hour11"));
+            $("#hour12 .task-item").val(localStorage.getItem("hour12"));
+            $("#hour13 .task-item").val(localStorage.getItem("hour13"));
+            $("#hour14 .task-item").val(localStorage.getItem("hour14"));
+            $("#hour15 .task-item").val(localStorage.getItem("hour15"));
+            $("#hour16 .task-item").val(localStorage.getItem("hour16"));
+            $("#hour17 .task-item").val(localStorage.getItem("hour17"));
+
+
+      //saves the task by clicking the save button
+      $(".saveBtn").on("click", function () {
+        // console.log(this); 
+         let time = $(this).parent().attr("id");
+         let task = $(this).siblings(".task-item").val();
+         
+ 
+         //setting the time and task values in the local storage
+         localStorage.setItem(time, task);
+     })
+    //called the function
+    timeTracer();
+})
